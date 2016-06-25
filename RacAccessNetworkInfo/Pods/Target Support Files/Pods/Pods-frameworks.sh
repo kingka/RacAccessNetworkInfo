904fe99b6a70458bfb46d1edb497acae5c0648eb
ImanolZhang <jingkang_zhang@vandagroup.com>
2016-06-25 12:19:04 +0800

    pod 'AFNetworking', '~> 3.0'

--- RacAccessNetworkInfo/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ RacAccessNetworkInfo/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -84,11 +84,13 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods/AFNetworking.framework"
   install_framework "Pods/ReactiveCocoa.framework"
   install_framework "Pods/Result.framework"
   install_framework "Pods/SVProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods/AFNetworking.framework"
   install_framework "Pods/ReactiveCocoa.framework"
   install_framework "Pods/Result.framework"
   install_framework "Pods/SVProgressHUD.framework"
